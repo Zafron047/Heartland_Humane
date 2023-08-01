d5420c73b868d1dc049adcfe19a2c29f5e872533
Zafron047 <117600135+Zafron047@users.noreply.github.com>
2023-08-01 18:21:40 +0600

    Setup API to fetch data for homepage

--- src/components/API.js
+++ src/components/API.js
@@ -1,3 +1,3 @@
-const urlAPI = 'https://api.thecatapi.com/v1/images/search?limit=10&breed_ids=beng&api_key=live_fHm4ziidMtDDEaXXkXVrMtOgCD6HWztKAJhZFzWeML3OUFJTeBPV1VPlQCvXsjxs';
+const urlAPI = 'https://api.thecatapi.com/v1/breeds';
 
 export default urlAPI;
--- src/redux/homeReducer/HomeSlice.js
+++ src/redux/homeReducer/HomeSlice.js
@@ -1,11 +1,39 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import axios from 'axios';
+import urlAPI from '../../components/API';
+
+export const fetchKatBreeds = createAsyncThunk('fetch Kat Breeds from API',
+  async () => {
+    const response = await axios.get(urlAPI);
+    return response.data;
+  });
 
 const homeReducer = createSlice({
   name: 'AllKats',
   initialState: {
-    array: [0, 1, 2, 3],
+    breedList: [],
+    loading: false,
+    error: null,
   },
   reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchKatBreeds.pending, (state) => {
+        state.loading = true;
+      })
+      .addCase(fetchKatBreeds.fulfilled, (state, action) => {
+        loading = false;
+        state.breedList = action.payload.map((eachKat) => ({
+          id: eachKat.id,
+          name: eachKat.name,
+          url: eachKat.url,
+        }))
+      })
+      .addCase(fetchKatBreeds.rejected, (state) => {
+        state.loading = false;
+        state.error = action.error;
+      });
+  },
 });
 
 export default homeReducer;
