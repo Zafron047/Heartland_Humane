e077791b69ed215c8719c6b90a7bc893030d13fa
Zafron047 <117600135+Zafron047@users.noreply.github.com>
2023-08-03 22:39:14 +0600

    Add filter functionality to use search bar.

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { fetchKatBreeds } from '../redux/homeReducer/HomeSlice';
@@ -13,6 +13,15 @@ const Home = () => {
     dispatch(fetchKatBreeds());
   }, [dispatch]);
 
+  const [searchQuery, setSearchQuery] = useState('');
+
+  const handleSearchInputChange = (event) => {
+    setSearchQuery(event.target.value);
+  };
+
+  const filteredBreeds = breedList.filter((breed) => breed.name
+    .toLowerCase().includes(searchQuery.toLowerCase()));
+
   let content;
 
   if (loading) {
@@ -21,28 +30,36 @@ const Home = () => {
     content = <p>{error}</p>;
   } else {
     content = (
-      <ul>
-        {breedList.map((breed) => (
-          <li key={breed.id}>
-            <Link to={`/Details/${breed.id}`}>
-              <div>
-                <h2>
-                  Breed:
-                  {breed.name}
-                </h2>
-                <p>
-                  Origin:
-                  {breed.origin}
-                </p>
-                <p>
-                  Weight:
-                  {breed.imperial}
-                </p>
-              </div>
-            </Link>
-          </li>
-        ))}
-      </ul>
+      <>
+        <input
+          type="text"
+          placeholder="Search by breed name..."
+          value={searchQuery}
+          onChange={handleSearchInputChange}
+        />
+        <ul>
+          {filteredBreeds.map((breed) => (
+            <li key={breed.id}>
+              <Link to={`/Details/${breed.id}`}>
+                <div>
+                  <h2>
+                    Breed:
+                    {breed.name}
+                  </h2>
+                  <p>
+                    Origin:
+                    {breed.origin}
+                  </p>
+                  <p>
+                    Weight:
+                    {breed.imperial}
+                  </p>
+                </div>
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </>
     );
   }
 
