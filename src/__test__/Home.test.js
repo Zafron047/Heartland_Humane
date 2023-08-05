660e208de03e97e48cd1f15e89aa0a291b691004
Zafron047 <117600135+Zafron047@users.noreply.github.com>
2023-08-05 14:51:40 +0600

    Add tests for home and details

--- src/__test__/Home.test.js
+++ src/__test__/Home.test.js
@@ -1,70 +1,18 @@
-// Home.test.js
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import { Provider } from 'react-redux';
-import thunk from 'redux-thunk';
-import configureMockStore from 'redux-mock-store';
-import axios from 'axios';
-import MockAdapter from 'axios-mock-adapter';
+import store from '../redux/store';
 import Home from '../components/Home';
-import '@testing-library/jest-dom/extend-expect';
 
-const axiosMock = new MockAdapter(axios);
-const middlewares = [thunk];
-const mockStore = configureMockStore(middlewares);
+const customRender = (component) => render(
+  <Provider store={store}>
+    {component}
+  </Provider>,
+);
 
-describe('Home Component', () => {
-  let store;
-
-  beforeEach(() => {
-    store = mockStore({
-      AllKats: {
-        breedList: [
-          { id: 1, name: 'Mock Cat 1', origin: 'Mock Origin 1' },
-          { id: 2, name: 'Mock Cat 2', origin: 'Mock Origin 2' },
-        ],
-        loading: false,
-        error: null,
-      },
-    });
-  });
-
-  it('should display loading when loading data', () => {
-    store = mockStore({
-      AllKats: {
-        breedList: [],
-        loading: true,
-        error: null,
-      },
-    });
-
-    render(
-      <Provider store={store}>
-        <Home />
-      </Provider>
-    );
-
-    const loadingText = screen.getByText('Loading...');
-    expect(loadingText).toBeInTheDocument();
-  });
-
-  it('should display error message when there is an error', () => {
-    store = mockStore({
-      AllKats: {
-        breedList: [],
-        loading: false,
-        error: 'Failed to fetch data',
-      },
-    });
-
-    render(
-      <Provider store={store}>
-        <Home />
-      </Provider>
-    );
-
-    const errorText = screen.getByText('Failed to fetch data');
-    expect(errorText).toBeInTheDocument();
+describe('Home', () => {
+  it('render Home Components', () => {
+    customRender(<Home />);
+    expect(Home).toMatchSnapshot();
   });
 });
-
